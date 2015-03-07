d1700740783872bd4d1dcd117a44e740c1e4e989
Seth Root <sethElijah@gmail.com>
2015-03-07 10:05:54 -0600

    Boards

--- LyneSolver/Boards.swift
+++ LyneSolver/Boards.swift
@@ -7,3 +7,28 @@
 //
 
 import Foundation
+
+var board1:Board {
+    var b = Matrix(rows: 4, columns: 4, repeatedValue: Node.Empty)
+    b[1,0] = Node.Start
+    b[1,3] = Node.End
+    b[2,0] = Node.Node
+    b[2,1] = Node.Node
+    b[2,2] = Node.Node
+    b[2,3] = Node.Node
+    return b
+}
+
+var board2:Board {
+    var b = Matrix(rows: 6, columns: 6, repeatedValue: Node.Empty)
+    b[0,0] = Node.Start
+    b[5,5] = Node.End
+    b[1,1] = Node.Node
+    b[2,2] = Node.Node
+    b[2,3] = Node.Node
+    b[3,3] = Node.Node
+    b[3,4] = Node.Node
+    b[3,5] = Node.Node
+    b[4,5] = Node.Node
+    return b
+}

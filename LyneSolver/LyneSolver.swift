5064d94b61c6eecd391bc4195410291cfe7e3068
Seth Root <sethElijah@gmail.com>
2015-03-07 10:06:30 -0600

    refactored LyneSolver

--- LyneSolver/LyneSolver.swift
+++ LyneSolver/LyneSolver.swift
@@ -11,64 +11,12 @@ import Foundation
 @objc
 class LyneSolver {
     
-    typealias Row = Int
-    typealias Col = Int
-    typealias Board = Matrix<Node>
-    typealias Processed = Matrix<Bool>
-    typealias AddrMod = (Int -> Int)
-    
-    
-    typealias DirTree = RoseTree<(Direction)>
-    typealias DirMatrix = [[Direction]]
-    typealias DirList = [Direction]
-    typealias DirModTriple = (Direction, AddrMod, AddrMod)
-    
-    enum Node : String, Printable {
-        case Empty = "_"
-        case Start = "S"
-        case Node = "o"
-        case End = "E"
+    var directions:[DirModTriple] {
         
-        var description: String {
-            return self.rawValue
-        }
-    }
-
-    enum Direction : String, Printable {
-        case Up = "up"
-        case Down = "down"
-        case Left = "left"
-        case Right = "right"
-        case UpLeft = "upLeft"
-        case UpRight = "upRight"
-        case DownLeft = "downLeft"
-        case DownRight = "downRight"
-        
-        var description: String {
-            return self.rawValue
-        }
-    }
-    
-    struct Addr : Printable {
-        let row:Int, col:Int
-        var description: String {
-        return "(\(row.description), \(col.description))"
-        }
-        init(row:Int, col:Int) {
-            self.row = row
-            self.col = col
-        }
-        func translate(modRow:AddrMod, modCol:AddrMod) -> Addr {
-            return Addr(row:modRow(self.row), col:modCol(self.col))
-        }
-    }
-    
-    var dirs:[DirModTriple] {
-    
         func id(i:Int) -> Int {return i}
         func inc(i:Int) -> Int {return i+1}
         func dec(i:Int) -> Int {return i-1}
-            
+        
         return [
             (Direction.Up, dec, id),
             (Direction.Down, inc, id),
@@ -81,90 +29,60 @@ class LyneSolver {
         ]
     }
     
-    var board:Board {
-        var b = Matrix(rows: 4, columns: 4, repeatedValue: Node.Empty)
-        b[1,0] = Node.Start
-        b[1,3] = Node.End
-        b[2,0] = Node.Node
-        b[2,1] = Node.Node
-        b[2,2] = Node.Node
-        b[2,3] = Node.Node
-        return b
-    }
-    
-    var board2:Board {
-        var b = Matrix(rows: 6, columns: 6, repeatedValue: Node.Empty)
-        b[0,0] = Node.Start
-        b[5,5] = Node.End
-        b[1,1] = Node.Node
-        b[2,2] = Node.Node
-        b[2,3] = Node.Node
-        b[3,3] = Node.Node
-        b[3,4] = Node.Node
-        b[3,5] = Node.Node
-        b[4,5] = Node.Node
-        return b
-    }
-    
-    var processed:Processed {
-        return Matrix(rows: 6, columns: 6, repeatedValue: false)
-    }
-    
     func main() {
-        solveBoard(board);
+        solveBoard(board1);
         solveBoard(board2);
     }
     
     func solveBoard(board:Board) {
+        
         println(board.description)
-        if let startNode:Addr = addressForNode(board, node: Node.Start) {
-            let trees:[Box<DirTree>] = findTreesForNode(board, addr:startNode, processed:processed)
+        
+        // Find start node
+        if let startNode:Address = addressessForNode(board, node: Node.Start) {
             
-            for tree in trees {
-                var matrix:DirMatrix = tree.value.flatten()
-                //println("source: " + tree.value.description)
-                for path in matrix {
-                    println(path)
-                }
+            // Initialize matrix to track which nodes have been processed
+            var processed:Processed = Matrix(rows: board.rows, columns: board.columns, repeatedValue: false)
+            
+            // Generate a RoseTree of Directions with Node.Start as the root
+            let trees:[Box<DirTree>] = findTreesForNode(board, address:startNode, processed:processed)
+            
+            // Flatten each tree and accumulate
+            var paths:DirMatrix = []
+            trees.map {paths += $0.value.flatten()}
+            
+            for path in paths {
+                println(path)
             }
+            
             println()
+            
         } else {
             NSLog("Error! Could not find starting node on the board")
         }
     }
     
-    func addressForNode(board:Board, node:Node) -> Addr? {
-        for var i=0; i<board.rows; i++ {
-            for var j=0; j<board.columns; j++ {
-                let n:Node = board[i,j]
-                if n == node {
-                    return Addr(row: i, col: j)
-                }
-            }
-        }
-        
-        return nil
-    }
+    
 
-    func findTreesForNode(board:Board, addr:Addr, var processed:Processed) -> [Box<DirTree>] {
-        let (row, col) = (addr.row, addr.col)
+    func findTreesForNode(board:Board, address:Address, var processed:Processed) -> [Box<DirTree>] {
+        let (row, col) = (address.row, address.col)
         processed[row, col] = true
         
-        let nodeExists = {(addr:Addr) -> Bool in
-            let (row, col) = (addr.row, addr.col)
+        let nodeExists = {(address:Address) -> Bool in
+            let (row, col) = (address.row, address.col)
             let node = board.indexIsValidForRow(row, column:col) ? board[row, col] : Node.Empty
             return node != Node.Empty
         }
         
-        func generatePaths(modRow:AddrMod, modCol:AddrMod) -> [Box<DirTree>]? {
-            let neighborNode:Addr = addr.translate(modRow, modCol: modCol)
+        func generatePaths(modRow:AddressMod, modCol:AddressMod) -> [Box<DirTree>]? {
+            let neighborNode:Address = address.translate(modRow, modCol: modCol)
             
             return nodeExists(neighborNode) && !processed[neighborNode.row, neighborNode.col] ?
-                findTreesForNode(board, addr: neighborNode, processed: processed) : nil;
+                findTreesForNode(board, address: neighborNode, processed: processed) : nil;
         }
         
         var trees:[Box<DirTree>] = []
-        for (direction, modRow, modCol) in dirs {
+        for (direction, modRow, modCol) in directions {
             if let paths = generatePaths(modRow, modCol) {
                 trees.append(Box(RoseTree.Node(Box(direction), paths)))
             }
@@ -172,6 +90,20 @@ class LyneSolver {
         
         return trees
     }
+    
+    func addressessForNode(board:Board, node:Node) -> Address? {
+        for var i=0; i<board.rows; i++ {
+            for var j=0; j<board.columns; j++ {
+                let n:Node = board[i,j]
+                if n == node {
+                    return Address(row: i, col: j)
+                }
+            }
+        }
+        
+        return nil
+    }
+
     
     
 }

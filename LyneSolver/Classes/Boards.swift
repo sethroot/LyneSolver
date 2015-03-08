afd64e4d8092b2647ac17e25362cbaaf24220a0b
Seth Root <sethElijah@gmail.com>
2015-03-08 11:08:22 -0500

    initializing Boards with new Matrix.init

--- LyneSolver/Classes/Boards.swift
+++ LyneSolver/Classes/Boards.swift
@@ -8,27 +8,32 @@
 
 import Foundation
 
+let ˍ = Node.Empty
+let S = Node.Start
+let o = Node.Node
+let E = Node.End;
+
 var board1:Board {
-    var b = Matrix(rows: 4, columns: 4, repeatedValue: Node.Empty)
-    b[1,0] = Node.Start
-    b[1,3] = Node.End
-    b[2,0] = Node.Node
-    b[2,1] = Node.Node
-    b[2,2] = Node.Node
-    b[2,3] = Node.Node
-    return b
+    
+    var board = [
+        [ˍ, ˍ, ˍ, ˍ],
+        [S, ˍ, ˍ, E],
+        [o, o, o, o],
+        [ˍ, ˍ, ˍ, ˍ],
+    ]
+    return Matrix(values:board, gapValue: Node.Empty)
 }
 
 var board2:Board {
-    var b = Matrix(rows: 6, columns: 6, repeatedValue: Node.Empty)
-    b[0,0] = Node.Start
-    b[5,5] = Node.End
-    b[1,1] = Node.Node
-    b[2,2] = Node.Node
-    b[2,3] = Node.Node
-    b[3,3] = Node.Node
-    b[3,4] = Node.Node
-    b[3,5] = Node.Node
-    b[4,5] = Node.Node
-    return b
+    
+    var board = [
+        [S,ˍ,ˍ,ˍ,ˍ,ˍ],
+        [ˍ,o,ˍ,ˍ,ˍ,ˍ],
+        [ˍ,o,o,ˍ,ˍ,ˍ],
+        [ˍ,ˍ,o,o,ˍ,ˍ],
+        [ˍ,ˍ,o,o,o,o],
+        [ˍ,ˍ,ˍ,ˍ,ˍ,E],
+    ]
+    
+    return Matrix(values:board, gapValue: Node.Empty)
 }

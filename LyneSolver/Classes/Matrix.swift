66ebeeceb83aa61d2156431f589de9a61b624089
Seth Root <sethElijah@gmail.com>
2015-03-08 11:07:09 -0500

    adds Matrix initialization for [[T]]

--- LyneSolver/Classes/Matrix.swift
+++ LyneSolver/Classes/Matrix.swift
@@ -9,8 +9,11 @@
 import Foundation
 
 struct Matrix<T:Printable> : Printable {
+    
     let rows: Int, columns: Int
-    public var grid: [T]
+    
+    internal var grid: [T]
+    
     var description: String {
         var desc:String = "["
         for var i=0; i<self.rows; i++ {
@@ -33,14 +36,29 @@ struct Matrix<T:Printable> : Printable {
         desc += "]"
         return desc
     }
+    
     init(rows: Int, columns: Int, repeatedValue:T) {
         self.rows = rows
         self.columns = columns
-        grid = Array(count: rows * columns, repeatedValue:repeatedValue)
+        grid = Array(count: rows * columns, repeatedValue: repeatedValue)
     }
+    
+    init(values:[[T]], gapValue:T) {
+        self.rows = values.count
+        self.columns = values.reduce(0) {max($0, $1.count)}
+        
+        grid = Array<T>();
+        for rIndex in 0...self.rows - 1 {
+            for cIndex in 0...self.columns - 1 {
+                grid.insert(values[rIndex][cIndex], atIndex: (rIndex * self.columns) + cIndex)
+            }
+        }
+    }
+    
     func indexIsValidForRow(row: Int, column: Int) -> Bool {
         return row >= 0 && row < rows && column >= 0 && column < columns
     }
+    
     subscript(row: Int, column: Int) -> T {
         get {
             assert(indexIsValidForRow(row, column: column), "Index out of range")
